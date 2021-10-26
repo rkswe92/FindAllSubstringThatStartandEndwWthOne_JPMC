dd0f311d59b1747d4918ad9e4784aa512ea2d1a3
rkswe92 <ravikanthswe2020@gmail.com>
2021-10-26 00:57:52 -0700

    Add project files.

---
+++ FindAllSubstringThatStartandEndwWthOne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindAllSubstringThatStartandEndwWthOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubstringFinder obj = new SubstringFinder();
+            obj.FindSubString("00100101");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FindAllSubstringThatStartandEndwWthOne/SubstringFinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAllSubstringThatStartandEndwWthOne
+{
+   public class SubstringFinder
+    {
+        public void FindSubString( String actual, char start='1', char end='1')
+        {
+            int pivot =0;
+            int last = 0;
+            List<string> list = new List<string>();
+            while(pivot< actual.Length)
+            {
+                if(actual[pivot]=='1')
+                {
+                    last = pivot + 1;
+                    while(last < actual.Length)
+                    {
+                        if(actual[last] =='1')
+                        {
+                            list.Add(actual.Substring(pivot, last - pivot+1));
+                        }
+                        last++;
+                    }
+                    pivot++;
+                }
+                pivot++;
+            }
+            foreach(var s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
